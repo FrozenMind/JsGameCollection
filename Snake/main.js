86f0e2133ab262477f25a2b5ea29221140f2ceb1
FrozenMind <valentin.rebscher@live.de>
2017-02-03 22:01:53 +0100

    Snake is close to fully working -snake can move -food spawns -snake can eat food and grows -snake crashs when hit wall -score is shown as console.log --> label needed and click to start needed -snake hits it self without dying--> well be added soon -food isnt that perfect yet

--- Snake/main.js
+++ Snake/main.js
@@ -1,38 +1,79 @@
 var stage;
 var snake;
+var food;
 
 init();
 
 function init() {
     //init stage
     stage = new createjs.Stage("gameArea");
+    stage.canvas.style.background = "#000000" //black background
     //create Snake with a little snake to start
     snake = new Snake();
     snake.loadDefaultSnake();
-
+    //create food rect
+    food = new createjs.Shape();
+    food.graphics.beginFill(snake.color).drawRect(0, 0, snake.size, snake.size);
+    //start location is on top half to make sure its not on snake
+    food.x = Math.floor(Math.random() * stage.canvas.width - snake.size);
+    food.y = Math.floor(Math.random() * stage.canvas.height - snake.size);
+    stage.addChild(food);
     //create ticker
     createjs.Ticker.addEventListener("tick", tick);
-    createjs.Ticker.setInterval(1000 / 60);
+    createjs.Ticker.setFPS(5); //in this game fps means snake speed
+    //add key event listener
+    document.addEventListener("keydown", keyDown, false);
 }
 
 function tick() {
+    snake.move();
+    checkFoodHit();
+    checkLoss();
     stage.update();
 }
 
 //if key is pressed
 function keyDown(e) {
+    console.log(e);
     switch (e.keyCode) {
-        case "":
-
+        case 38: //38 = up
+            if (snake.direction != 3) {
+                snake.direction = 1;
+            }
+            break;
+        case 39: //39 = right
+            if (snake.direction != 4) {
+                snake.direction = 2;
+            }
+            break;
+        case 40: //40 = down
+            if (snake.direction != 1) {
+                snake.direction = 3;
+            }
+            break;
+        case 37: //37 = left
+            if (snake.direction != 2) {
+                snake.direction = 4;
+            }
             break;
     }
 }
 
-//if key is released
-function keyUp(e) {
-    switch (e.keyCode) {
-        case "":
+//if snake hits food make snake bigger and move food
+function checkFoodHit() {
+    if (food.y + snake.size >= snake.rects[0].y && food.y <= snake.rects[0].y + snake.size && food.x <= snake.rects[0].x + snake.size && food.x + snake.size >= snake.rects[0].x) {
+        food.x = Math.floor(Math.random() * (stage.canvas.width - snake.size));
+        food.y = Math.floor(Math.random() * (stage.canvas.height - snake.size));
+        snake.addRect();
+    }
+}
 
-            break;
+//check if snake is out of window
+function checkLoss() {
+
+    if (snake.rects[0].y < 0 || snake.rects[0].y + snake.size > stage.canvas.height || snake.rects[0].x < 0 || snake.rects[0].x + snake.size > stage.canvas.width) {
+        console.log("you lost. Your Score is: " + snake.rects.length);
+        //alert("you lost. Your Score is: " + snake.rects.length);
+        init();
     }
 }
--- Snake/snake.js
+++ Snake/snake.js
@@ -1,27 +1,56 @@
 function Snake() {
-    this.color = "#000000";
-    this.speed = 5;
-    this.size = 15;
+    this.color = "#ffffff"; //white snake
+    this.size = 20;
     this.rects = [];
-    this.direction; //1=up, 2=right, 3=down, 4=left
+    this.direction = 4; //1=up, 2=right, 3=down, 4=left
 
     this.loadDefaultSnake = function() {
         this.rects.push(new createjs.Shape());
-        this.rects[this.rects.length - 1].graphics.beginFill(this.color).drawRect(stage.canvas.width / 2 - 3 * this.size, stage.canvas.height / 2, this.size, this.size);
-        console.log(stage.canvas.width);
+        this.rects[this.rects.length - 1].graphics.beginFill(this.color).drawRect(0, 0, this.size, this.size);
+        this.rects[this.rects.length - 1].x = stage.canvas.width / 2 - 3 * this.size;
+        this.rects[this.rects.length - 1].y = stage.canvas.height / 2;
         stage.addChild(this.rects[this.rects.length - 1]);
         this.rects.push(new createjs.Shape());
-        this.rects[this.rects.length - 1].graphics.beginFill(this.color).drawRect(stage.canvas.width / 2 - 2 * this.size, stage.canvas.height / 2, this.size, this.size);
+        this.rects[this.rects.length - 1].graphics.beginFill(this.color).drawRect(0, 0, this.size, this.size);
+        this.rects[this.rects.length - 1].x = stage.canvas.width / 2 - 2 * this.size;
+        this.rects[this.rects.length - 1].y = stage.canvas.height / 2;
         stage.addChild(this.rects[this.rects.length - 1]);
         this.rects.push(new createjs.Shape());
-        this.rects[this.rects.length - 1].graphics.beginFill(this.color).drawRect(stage.canvas.width / 2 - this.size, stage.canvas.height / 2, this.size, this.size);
+        this.rects[this.rects.length - 1].graphics.beginFill(this.color).drawRect(0, 0, this.size, this.size);
+        this.rects[this.rects.length - 1].x = stage.canvas.width / 2 - this.size;
+        this.rects[this.rects.length - 1].y = stage.canvas.height / 2;
         stage.addChild(this.rects[this.rects.length - 1]);
         this.direction = 4;
     };
 
-    this.addRect = function(x, y) {
+    this.addRect = function() {
         this.rects.push(new createjs.Shape());
-        this.rects[this.rects.length - 1].graphics.beginFill(this.color).drawRect(x, y, this.size, this.size);
+        this.rects[this.rects.length - 1].graphics.beginFill(this.color).drawRect(0, 0, this.size, this.size);
+        this.rects[this.rects.length - 1].x = this.rects[this.rects.length - 1].x + this.size;
+        this.rects[this.rects.length - 1].y = this.rects[this.rects.length - 1].y + this.size;
         stage.addChild(this.rects[this.rects.length - 1]);
     };
+
+    this.move = function() {
+        //every rect should be set to the one before him
+        for (i = this.rects.length - 1; i >= 1; i--) {
+            this.rects[i].x = this.rects[i - 1].x;
+            this.rects[i].y = this.rects[i - 1].y;
+        }
+        //rect 0 should get a new position
+        switch (this.direction) {
+            case 1: //up
+                this.rects[0].y -= this.size;
+                break;
+            case 2: //right
+                this.rects[0].x += this.size;
+                break;
+            case 3: //down
+                this.rects[0].y += this.size;
+                break;
+            case 4: //left
+                this.rects[0].x -= this.size;
+                break;
+        }
+    }
 }
