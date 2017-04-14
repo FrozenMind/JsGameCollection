739347eb49c311e2566ccb7e9b5aad2954e378c1
Valentin aka FrozenMind <valentin.rebscher@games-broduction.com>
2017-04-14 17:20:18 +0200

    add: lib/game.js
    both socket to construnctor
    isread function: to control if players are rdy

--- Pong/lib/game.js
+++ Pong/lib/game.js
@@ -1,9 +1,21 @@
 //Game File to create gameobject
 
-module.exports = Game;
-
 var Game = function(socket1, socket2) {
+    this.s1 = socket1;
+    this.s2 = socket2;
+    this.ready = 0;
     //TODO: init players and ball
+}
+
+Game.prototype.isReady = function(name) {
+    if (this.s1.name == name || this.s2.name == name) {
+        this.ready++;
+    }
+    if (this.ready == 2) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 Game.prototype.getGameObject = function() {
@@ -33,3 +45,5 @@ Game.prototype.update = function() {
 Game.prototype.movePlayer = function(socket, upOrDown) {
     //TODO: move player up or down depends on what key he send
 }
+
+module.exports = Game;
