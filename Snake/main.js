76e9c4bfe6ee5f63bd6d0f0a8d2f9a978fb93cba
FrozenMind <valentin.rebscher@live.de>
2017-10-05 14:52:46 +0200

    improvement / layout

--- Snake/main.js
+++ Snake/main.js
@@ -7,147 +7,147 @@ var fps;
 var score = 0;
 var textOverlay;
 
-$(document).ready(function () {
-    init();
+$(document).ready(function() {
+  init();
 });
 
 function init() {
-    fps = 60;
-    //init stage
-    stage = new createjs.Stage("gameArea");
-    stage.canvas.style.background = "#000000" //black background
-        //create ticker
-    createjs.Ticker.addEventListener("tick", tick);
-    createjs.Ticker.setFPS(fps); //in this game fps means snake speed
-    createjs.Ticker.paused = true;
+  fps = 60;
+  //init stage
+  stage = new createjs.Stage("gameArea");
+  stage.canvas.style.background = "#000000" //black background
+  //create ticker
+  createjs.Ticker.addEventListener("tick", tick);
+  createjs.Ticker.setFPS(fps); //in this game fps means snake speed
+  createjs.Ticker.paused = true;
 
-    // Overlay 
-    textOverlay = new createjs.Text();
-    textOverlay.set({
-        text: "Press any key to start \n Use arrow keys or WASD to move!",
-        font: "24px Arial",
-        color: "#ffffff",
-        textAlign: "center",
-        textBaseline: "middle",
-        x: stage.canvas.width / 2,
-        y: stage.canvas.height / 2
-    });
-    stage.addChild(textOverlay);
-    stage.update();
+  // Overlay
+  textOverlay = new createjs.Text();
+  textOverlay.set({
+    text: "Press any key to start \n Use arrow keys or WASD to move!",
+    font: "24px Arial",
+    color: "#ffffff",
+    textAlign: "center",
+    textBaseline: "middle",
+    x: stage.canvas.width / 2,
+    y: stage.canvas.height / 2
+  });
+  stage.addChild(textOverlay);
+  stage.update();
 
-    //add key event listener
-    document.addEventListener("keydown", keyDown, false);
+  //add key event listener
+  document.addEventListener("keydown", keyDown, false);
 }
 
 function start() {
-    score = 0
-    stage.removeAllChildren();
-    //create snake and load default
-    snake = new Snake();
-    snake.loadDefaultSnake();
-    //create food rect
-    food = new createjs.Shape();
-    food.graphics.beginFill('#006600').drawRect(0, 0, snake.size, snake.size);
-    //start location is on top half to make sure its not on snake
-    food.x = Math.floor(Math.random() * (stage.canvas.width / snake.size)) * snake.size;
-    food.y = Math.floor(Math.random() * (stage.canvas.height / snake.size)) * snake.size;
-    stage.addChild(food);
-    //set food in background
-    stage.setChildIndex(food, 0);
-    createjs.Ticker.paused = false;
+  score = 0
+  stage.removeAllChildren();
+  //create snake and load default
+  snake = new Snake();
+  snake.loadDefaultSnake();
+  //create food rect
+  food = new createjs.Shape();
+  food.graphics.beginFill('#ffa400').drawRect(0, 0, snake.size, snake.size);
+  //start location is on top half to make sure its not on snake
+  food.x = Math.floor(Math.random() * (stage.canvas.width / snake.size)) * snake.size;
+  food.y = Math.floor(Math.random() * (stage.canvas.height / snake.size)) * snake.size;
+  stage.addChild(food);
+  //set food in background
+  stage.setChildIndex(food, 0);
+  createjs.Ticker.paused = false;
 }
 
 function tick(event) {
-    if (event.paused)
-        return;
+  if (event.paused)
+    return;
 
-    if (speedCounter >= snake.speed) {
-        speedCounter = 0;
-        snake.move();
-        stage.update();
-        snake.calcSpeed();
-        checkFoodHit();
-        isGameLost();
-    }
-    speedCounter++;
+  if (speedCounter >= snake.speed) {
+    speedCounter = 0;
+    snake.move();
+    stage.update();
+    checkFoodHit();
+    isGameLost();
+  }
+  speedCounter++;
 }
 
 //if key is pressed
 function keyDown(e) {
-    if (!createjs.Ticker.paused) {
-        if (snake.readyToMove) {
-            switch (e.keyCode) {
-            case 38: //38 = up
-            case 87: //87 = w
-                if (snake.direction != 3) {
-                    snake.direction = 1;
-                    snake.readyToMove = false;
-                }
-                break;
-            case 39: //39 = right
-            case 68: //39 = d
-                if (snake.direction != 4) {
-                    snake.direction = 2;
-                    snake.readyToMove = false;
-                }
-                break;
-            case 40: //40 = down
-            case 83: //40 = s
-                if (snake.direction != 1) {
-                    snake.direction = 3;
-                    snake.readyToMove = false;
-                }
-                break;
-            case 37: //37 = left
-            case 65: //37 = a
-                if (snake.direction != 2) {
-                    snake.direction = 4;
-                    snake.readyToMove = false;
-                }
-                break;
-            }
-        }
-    } else {
-        //press any key to start
-        start();
+  if (!createjs.Ticker.paused) {
+    if (snake.readyToMove) {
+      switch (e.keyCode) {
+        case 38: //38 = up
+        case 87: //87 = w
+          if (snake.direction != 3) {
+            snake.direction = 1;
+            snake.readyToMove = false;
+          }
+          break;
+        case 39: //39 = right
+        case 68: //39 = d
+          if (snake.direction != 4) {
+            snake.direction = 2;
+            snake.readyToMove = false;
+          }
+          break;
+        case 40: //40 = down
+        case 83: //40 = s
+          if (snake.direction != 1) {
+            snake.direction = 3;
+            snake.readyToMove = false;
+          }
+          break;
+        case 37: //37 = left
+        case 65: //37 = a
+          if (snake.direction != 2) {
+            snake.direction = 4;
+            snake.readyToMove = false;
+          }
+          break;
+      }
     }
+  } else {
+    //press any key to start
+    start();
+  }
 }
 
 //if snake hits food make snake bigger and move food
 function checkFoodHit() {
-    if (food.y == snake.rects[0].y && food.x == snake.rects[0].x) {
-        food.x = Math.floor(Math.random() * (stage.canvas.width / snake.size)) * snake.size;
-        food.y = Math.floor(Math.random() * (stage.canvas.height / snake.size)) * snake.size;
-        snake.addRect();
-        score++;
-    }
+  if (food.y == snake.rects[0].y && food.x == snake.rects[0].x) {
+    food.x = Math.floor(Math.random() * (stage.canvas.width / snake.size)) * snake.size;
+    food.y = Math.floor(Math.random() * (stage.canvas.height / snake.size)) * snake.size;
+    snake.addRect();
+    score++;
+    snake.calcSpeed(); //calc new speed on food hit
+  }
 }
 
 //check if game is lost
 function isGameLost() {
-    var headPosX = snake.rects[0].x;
-    var headPosY = snake.rects[0].y;
+  var headPosX = snake.rects[0].x;
+  var headPosY = snake.rects[0].y;
 
-    // true if snake hits a wall
-    var wallHitted = headPosY < 0 || // top wall
-        headPosY + snake.size > stage.canvas.height || // bottom wall
-        headPosX < 0 || // left wall
-        headPosX + snake.size > stage.canvas.width; // right wall
+  // true if snake hits a wall
+  var wallHitted = headPosY < 0 || // top wall
+    headPosY + snake.size > stage.canvas.height || // bottom wall
+    headPosX < 0 || // left wall
+    headPosX + snake.size > stage.canvas.width; // right wall
 
-    // true if snake hits itself
-    var selfHitted = false;
-    for (var i = 2; i < snake.rects.length; i++) {
-        selfHitted = headPosX == snake.rects[i].x && headPosY == snake.rects[i].y;
-        if (selfHitted)
-            break;
-    }
+  // true if snake hits itself
+  var selfHitted = false;
+  for (var i = 2; i < snake.rects.length; i++) {
+    selfHitted = headPosX == snake.rects[i].x && headPosY == snake.rects[i].y;
+    if (selfHitted)
+      break;
+  }
 
-    if (wallHitted || selfHitted) {
-        createjs.Ticker.paused = true;
-        stage.addChild(textOverlay);
-        stage.update();
-        $("#ResultScore").html("<b>You lost. Your Score is: " + score + "</b>")
-    } else {
-        $("#ResultScore").html("<b>Score: " + score + "</b>")
-    }
+  if (wallHitted || selfHitted) {
+    createjs.Ticker.paused = true;
+    stage.addChild(textOverlay);
+    stage.update();
+    $("#ResultScore").html("<b>You lost. Your Score is: " + score + "</b>")
+  } else {
+    $("#ResultScore").html("<b>Score: " + score + "</b>")
+  }
 }
--- Snake/snake.js
+++ Snake/snake.js
@@ -1,68 +1,73 @@
 function Snake() {
-    this.color = "#0000b3"; //white snake
-    this.headColor = "#ff0000"; //head is read
-    this.size = 20;
-    this.rects = [];
-    this.direction = 4; //1=up, 2=right, 3=down, 4=left
-    this.speed = 15; //speed can be max fps count
-    this.readyToMove = true;
+  this.color = "#ccc"; //white snake
+  this.headColor = "#ff0000"; //head is read
+  this.size = 20;
+  this.rects = [];
+  this.direction = 4; //1=up, 2=right, 3=down, 4=left
+  this.speed = 15; //speed can be max fps count
+  this.readyToMove = true
+  this.startAmount = 7 //amout of elements in the start snake
 }
 
-Snake.prototype.loadDefaultSnake = function () {
-    this.rects.push(new createjs.Shape());
-    this.rects[this.rects.length - 1].graphics.beginFill(this.headColor).drawRect(0, 0, this.size, this.size);
-    this.rects[this.rects.length - 1].x = stage.canvas.width / 2 - 3 * this.size;
-    this.rects[this.rects.length - 1].y = stage.canvas.height / 2;
-    stage.addChild(this.rects[this.rects.length - 1]);
-    this.rects.push(new createjs.Shape());
-    this.rects[this.rects.length - 1].graphics.beginFill(this.color).drawRect(0, 0, this.size, this.size);
-    this.rects[this.rects.length - 1].x = stage.canvas.width / 2 - 2 * this.size;
-    this.rects[this.rects.length - 1].y = stage.canvas.height / 2;
-    stage.addChild(this.rects[this.rects.length - 1]);
-    this.rects.push(new createjs.Shape());
-    this.rects[this.rects.length - 1].graphics.beginFill(this.color).drawRect(0, 0, this.size, this.size);
-    this.rects[this.rects.length - 1].x = stage.canvas.width / 2 - this.size;
-    this.rects[this.rects.length - 1].y = stage.canvas.height / 2;
-    stage.addChild(this.rects[this.rects.length - 1]);
-    this.direction = 4;
+Snake.prototype.loadDefaultSnake = function() {
+  //head
+  let tempRect = new createjs.Shape()
+  tempRect.graphics.setStrokeStyle(1).beginStroke("#000")
+  tempRect.graphics.beginFill(this.headColor).drawRect(0, 0, this.size, this.size);
+  tempRect.x = stage.canvas.width / 2 - this.startAmount * this.size;
+  tempRect.y = stage.canvas.height / 2;
+  this.rects.push(tempRect);
+  stage.addChild(tempRect);
+  //tail
+  for (let i = this.startAmount - 1; i > 0; i--) {
+    let tempRect = new createjs.Shape()
+    tempRect.graphics.setStrokeStyle(1).beginStroke("#000")
+    tempRect.graphics.beginFill(this.color).drawRect(0, 0, this.size, this.size);
+    tempRect.x = stage.canvas.width / 2 - i * this.size;
+    tempRect.y = stage.canvas.height / 2;
+    this.rects.push(tempRect);
+    stage.addChild(tempRect);
+  }
+  this.direction = 4;
 }
 
-Snake.prototype.addRect = function () {
-    this.rects.push(new createjs.Shape());
-    this.rects[this.rects.length - 1].graphics.beginFill(this.color).drawRect(0, 0, this.size, this.size);
-    // first set is out of bounds so that no flickr appears
-    this.rects[this.rects.length - 1].x = -this.size;
-    this.rects[this.rects.length - 1].y = -this.size;
-    stage.addChild(this.rects[this.rects.length - 1]);
+Snake.prototype.addRect = function() {
+  let tempRect = new createjs.Shape()
+  tempRect.graphics.setStrokeStyle(1).beginStroke("#000")
+  tempRect.graphics.beginFill(this.color).drawRect(0, 0, this.size, this.size);
+  // first set is out of bounds so that no flickr appears, move method will set it to the end
+  tempRect.x = -this.size;
+  tempRect.y = -this.size;
+  this.rects.push(tempRect)
+  stage.addChild(tempRect)
 }
 
-Snake.prototype.move = function () {
-    //every rect should be set to the one before him
-    for (i = this.rects.length - 1; i >= 1; i--) {
-        this.rects[i].x = this.rects[i - 1].x;
-        this.rects[i].y = this.rects[i - 1].y;
-    }
-    //rect 0 should get a new position
-    switch (this.direction) {
+Snake.prototype.move = function() {
+  //every rect should be set to the one before him
+  for (i = this.rects.length - 1; i >= 1; i--) {
+    this.rects[i].x = this.rects[i - 1].x;
+    this.rects[i].y = this.rects[i - 1].y;
+  }
+  //rect 0 should get a new position
+  switch (this.direction) {
     case 1: //up
-        this.rects[0].y -= this.size;
-        break;
+      this.rects[0].y -= this.size;
+      break;
     case 2: //right
-        this.rects[0].x += this.size;
-        break;
+      this.rects[0].x += this.size;
+      break;
     case 3: //down
-        this.rects[0].y += this.size;
-        break;
+      this.rects[0].y += this.size;
+      break;
     case 4: //left
-        this.rects[0].x -= this.size;
-        break;
-    }
-    this.readyToMove = true;
+      this.rects[0].x -= this.size;
+      break;
+  }
+  this.readyToMove = true;
 }
 
 //calculate the speed by the size of the snake
-Snake.prototype.calcSpeed = function () {
-    if (this.speed > 3) {
-        this.speed = 15 - Math.floor(this.rects.length / 3);
-    }
+Snake.prototype.calcSpeed = function() {
+  if (this.speed > 3)
+    this.speed = 15 - Math.floor(this.rects.length / 3)
 }
